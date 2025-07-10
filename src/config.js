785b7fc56d978f3d34d949ac8f15db11c6e25143
Kunal Thakur <kunalkharyal@gmail.com>
2025-07-10 16:40:44 +0530

    production

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    API_URL: process.env.REACT_APP_API_URL,
+    // API_URL: process.env.REACT_APP_API_URL,
+    API_URL: 'http://103.185.212.117:4550/',
 };
 
 export default config;
